a62acdfa989a16a131a932eaa01fa4711cb536a7
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-02-15 21:52:34 +0100

    WindowsList added

---
+++ c-sharp.0x09/WindowsList/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WindowsList;
+using HWND = IntPtr;
+
+static class Program
+{
+    static void Main()
+    {
+        foreach(KeyValuePair<IntPtr, string> window in OpenWindowGetter.GetOpenWindows())
+        {
+            IntPtr handle = window.Key;
+            string title = window.Value;
+
+            Console.WriteLine("{0}: {1}", handle, title);
+        }
+    }
+    
+    /// <summary>Contains functionality to get all the open windows.</summary>
+    /// @NOTE: Imported from https://www.tcx.be/blog/2006/list-open-windows/
+    public static class OpenWindowGetter
+    {
+        /// <summary>Returns a dictionary that contains the handle and title of all the open windows.</summary>
+        /// <returns>A dictionary that contains the handle and title of all the open windows.</returns>
+        public static IDictionary<HWND, string> GetOpenWindows()
+        {
+            HWND shellWindow = GetShellWindow();
+            Dictionary<HWND, string> windows = new Dictionary<HWND, string>();
+
+            EnumWindows(delegate(HWND hWnd, int lParam)
+            {
+                if (hWnd == shellWindow) return true;
+                if (!IsWindowVisible(hWnd)) return true;
+
+                int length = GetWindowTextLength(hWnd);
+                if (length == 0) return true;
+
+                StringBuilder builder = new StringBuilder(length);
+                GetWindowText(hWnd, builder, length + 1);
+
+                windows[hWnd] = builder.ToString();
+                return true;
+
+            }, 0);
+
+            return windows;
+        }
+
+        private delegate bool EnumWindowsProc(HWND hWnd, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowText(HWND hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowTextLength(HWND hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool IsWindowVisible(HWND hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern IntPtr GetShellWindow();
+    }
+}
